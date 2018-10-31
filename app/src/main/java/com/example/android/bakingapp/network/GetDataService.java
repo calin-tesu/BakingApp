6ff0dd6618dd7269b28bb3f1b3a7247235cffcfe
calin-tesu <likareturneanu@gmail.com>
2018-10-31 10:46:22 +0200

    Create the Retrofit Instance Define the Endpoints

---
+++ app/src/main/java/com/example/android/bakingapp/network/GetDataService.java
@@ -0,0 +1,14 @@
+package com.example.android.bakingapp.network;
+
+import com.example.android.bakingapp.models.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/topher/2017/May/59121517_baking/baking.json")
+    Call<List<Recipe>> getAllRecipes();
+}
