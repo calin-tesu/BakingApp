0b08fdcd7c48b97e4d7000a11949a9eac90de57a
calin-tesu <likareturneanu@gmail.com>
2018-11-08 13:06:47 +0200

    setup ExoPlayer

--- app/src/main/java/com/example/android/bakingapp/fragments/StepFragment.java
+++ app/src/main/java/com/example/android/bakingapp/fragments/StepFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.bakingapp.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +12,18 @@ import android.widget.TextView;
 
 import com.example.android.bakingapp.R;
 import com.example.android.bakingapp.models.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +38,10 @@ public class StepFragment extends Fragment {
     @BindView(R.id.step_description)
     TextView stepDescription;
     Step currentStep;
+
+    @BindView(R.id.player_view)
+    SimpleExoPlayerView mPlayerView;
+    private SimpleExoPlayer mExoPlayer;
 
     // Mandatory empty constructor
     public StepFragment() {
@@ -46,6 +63,40 @@ public class StepFragment extends Fragment {
         stepTitle.setText(currentStep.getShortDescription());
         stepDescription.setText(currentStep.getDescription());
 
+        Uri mediaUri = Uri.parse(currentStep.getVideoURL());
+        initializePlayer(mediaUri);
+
         return rootView;
+    }
+
+    /**
+     * Initialize ExoPlayer.
+     *
+     * @param mediaUri The URI of the sample to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getActivity(), "bakingapp");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri,
+                    new DefaultDataSourceFactory(getActivity(), userAgent),
+                    new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
     }
 }
